47e59edbabaaa193fda518d06b1eb058ef493b28
Siniša Panić <sinisa.panic@actual-it.si>
2022-12-18 23:57:56 +0100

    Implement add and remove funds from wallet

---
+++ addFunds.js
@@ -0,0 +1,16 @@
+const { pool } = require('./server');
+
+async function addFunds(addedFunds, wallet_id) {
+    var funds = parseInt(await (await pool.query(`SELECT funds FROM wallet WHERE wallet_id = ${wallet_id}`)).rows[0].funds);
+    var newFunds = funds + addedFunds;
+    const query = 'UPDATE wallet SET funds = $1 WHERE wallet_id = $2'
+    const data = [newFunds, wallet_id];
+    try {
+        pool.query(query, data);
+        console.log(`Updated the wallet funds to ${newFunds}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+addFunds(300, 1);
